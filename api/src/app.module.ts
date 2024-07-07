ae1f781c9a9a8a1a81aa05eef21b82957fffd2f7
Johnny Zavala <jzavalafgz@gmail.com>
2024-07-06 20:40:21 -0400

    Chore: Created new project using nestjs-api as base

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from './datasource/typeorm.module';
 
 @Module({
-  imports: [],
+  imports: [TodosModule, ConfigModule.forRoot(), TypeOrmModule],
   controllers: [AppController],
   providers: [AppService],
 })
